436a718912b3545f10bc481c1dbe6fecf0ac6bc5
Razvlekal0vka <u14111415232119518@gmail.com>
2022-03-08 22:05:06 +0300

    examenation.py - Сделана заготовка загрузочной полоскт

--- examination.py
+++ examination.py
@@ -55,6 +55,7 @@ class Board:
         self.k = 0
         self.k1 = 0
         self.font = pygame.font.SysFont("Comic MS", 20)
+        self.coord_loading = [0, 14]
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -65,17 +66,19 @@ class Board:
     def render(self, screen):
         for i in range(self.width):
             for j in range(self.height):
-                if self.board[j][i] == 665:
+                if self.board[j][i] == 666:
                     pygame.draw.rect(screen,
                                      (60, 63, 65),
                                      (self.left + self.cell_size * i,
                                       self.top + self.cell_size * j,
                                       self.cell_size, self.cell_size), 0)
 
-                elif self.board[j][i] == 666:
-                    num = self.board1[j][i]
-                    screen.blit(self.font.render(str(num), 1, (60, 63, 65)), (self.left + self.cell_size * i,
-                                                                               self.top + self.cell_size * j))
+                elif self.board[j][i] == 1:
+                    pygame.draw.rect(screen,
+                                     (198, 117, 49),
+                                     (self.left + self.cell_size * i,
+                                      self.top + self.cell_size * j,
+                                      self.cell_size, self.cell_size), 6)
 
                 else:
                     pygame.draw.rect(screen,
@@ -84,7 +87,8 @@ class Board:
                                       self.top + self.cell_size * j,
                                       self.cell_size, self.cell_size), 6)
 
-                pygame.draw.rect(screen,(43, 43, 43),(self.left + self.cell_size * i,self.top + self.cell_size * j,self.cell_size, self.cell_size), 3)
+                pygame.draw.rect(screen, (43, 43, 43), (
+                self.left + self.cell_size * i, self.top + self.cell_size * j, self.cell_size, self.cell_size), 3)
 
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
@@ -103,76 +107,27 @@ class Board:
                 self.k = 2
                 self.board[cell_coords[1]][cell_coords[0]] = 2
         print(cell_coords)
-        board.update(cell_coords)
 
-    def update(self, ck):
-        temp_board = copy.deepcopy(self.board)
-        temp_board1 = copy.deepcopy(self.board)
-        for i in range(self.width):
-            for j in range(self.height):
-                x1, y1 = i - 1, j - 1
-                x2, y2 = i + 1, j + 1
-
-                #
-
-                neib = 0
-
-                if y1 >= 0:
-                    if self.board[y1][i] != 2:
-                        neib += self.board[y1][i]
-
-                if y2 <= self.height - 1:
-                    if self.board[y2][i] != 2:
-                        neib += self.board[y2][i]
-
-                if x1 >= 0:
-                    if self.board[j][x1] != 2:
-                        neib += self.board[j][x1]
-
-                if x2 <= self.width - 1:
-                    if self.board[j][x2] != 2:
-                        neib += self.board[j][x2]
-
-                if y1 >= 0 and x1 >= 0:
-                    if self.board[y1][x1] != 2:
-                        neib += self.board[y1][x1]
-
-                if y1 >= 0 and x2 <= self.width - 1:
-                    if self.board[y1][x2] != 2:
-                        neib += self.board[y1][x2]
-
-                if y2 <= self.height - 1 and x1 >= 0:
-                    if self.board[y2][x1] != 2:
-                        neib += self.board[y2][x1]
-
-                if y2 <= self.height - 1 and x2 <= self.width - 1:
-                    if self.board[y2][x2] != 2:
-                        neib += self.board[y2][x2]
-
-                temp_board1[j][i] = neib
-        for i in range(self.width):
-            for j in range(self.height):
-                self.board1[j][i] = temp_board1[j][i]
-
-    def opp(self):
-        for _ in range(random.randint(2, 15)):
-            cell_coords = (random.randint(0, 8), random.randint(0, 8))
-            if cell_coords:
-                self.board[cell_coords[1]][cell_coords[0]] = 1
+    def loading(self, tic):
+        self.coord_loading = [self.coord_loading[0] + 1, self.coord_loading[1]]
+        if self.coord_loading[0] <= 26:
+            self.board[self.coord_loading[1]][self.coord_loading[0]] = 1
 
 
 board = Board(num_cell_x, num_cell_y, cell_size, shift_left, shift_top)
+k = 0
 running = True
 op = False
 while running:
+    k += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             board.get_click(event.pos)
-        if not op:
-            board.opp()
             op = True
+    if k % 60 == 0:
+        board.loading(k)
     screen.fill((43, 43, 43))
     board.render(screen)
     pygame.display.flip()
