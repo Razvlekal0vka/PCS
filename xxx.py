6c9ce59ea5f71486113d5a4b02722e46d651ac08
Razvlekal0vka <u14111415232119518@gmail.com>
2022-03-14 14:30:25 +0300

    .

--- xxx.py
+++ xxx.py
@@ -1,17 +1,2 @@
-def loading(self, step_examination):
-    number_of_cells = self.number_of_cells
-    col_examination = self.col_examination
-    self.percent += (step_examination / col_examination - self.percent)
-    self.percent_for_step = col_examination / number_of_cells
-
-    print(self.percent_for_step)
-    print(self.percent)
-
-    if self.percent >= self.percent_for_step:
-        self.percent -= self.percent_for_step
-        self.coord_loading = [self.coord_loading[0] + 1, self.coord_loading[1]]
-        if self.coord_loading[0] <= 26:
-            self.board[self.coord_loading[1]][self.coord_loading[0]] = 1
-        if self.coord_loading[0] == 26:
-            pass
-            '''KILPROC'''
+import easygui
+print(easygui.fileopenbox(filetypes=["*.docx"]))
