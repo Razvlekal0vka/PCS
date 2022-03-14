aaac64cc6116446b881c99ae515d5c3ed4c8d3da
Razvlekal0vka <u14111415232119518@gmail.com>
2022-03-14 13:35:27 +0300

    .

--- PCS.py
+++ PCS.py
@@ -17,16 +17,21 @@ class Example(QWidget):
             if num_line == 2:
                 if line == '800*450' or line == '1280*720' or line == '1600*900' or line == '1920*1080' or \
                             line == '2048*1152' or line == '3840*2160':
+                    self.pcs_resolution = line.split('*')
+            if num_line == 1:
+                if line == '800*450' or line == '1280*720' or line == '1600*900' or line == '1920*1080' or \
+                        line == '2048*1152' or line == '3840*2160':
                     self.screen_resolution = line.split('*')
-                    print(self.screen_resolution)
 
             self.read_data.append(line)
             num_line += 1
 
+        self.shift_x = (int(self.screen_resolution[0]) / 2) - (int(self.pcs_resolution[0]) / 2)
+        self.shift_y = (int(self.screen_resolution[1]) / 2) - (int(self.pcs_resolution[1]) / 2)
+
     def initUI(self):
         self.read()
-        print(int(self.screen_resolution[0]), int(self.screen_resolution[1]))
-        self.setGeometry(100, 100, int(self.screen_resolution[0]), int(self.screen_resolution[1]))
+        self.setGeometry(int(self.shift_x), int(self.shift_y), int(self.pcs_resolution[0]), int(self.pcs_resolution[1]))
         self.setWindowTitle('Personal Cloud Sync')
 
 
