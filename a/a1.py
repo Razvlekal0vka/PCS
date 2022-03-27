456c578f02d9a53991f261b5a2f9036d75f67530
Razvlekal0vka <u14111415232119518@gmail.com>
2022-03-27 14:55:46 +0300

    .

--- a/a1.py
+++
@@ -1,8 +0,0 @@
-a, b = list(map(int, input().split()))
-n, m = list(map(int, input().split()))
-c1 = 0
-c2 = b * m
-while c1 + c2 != n:
-    c2 -= b
-    c1 += a
-print(c1, c2)
--- v/1v.py
+++
@@ -1,8 +0,0 @@
-a, b = list(map(int, input().split()))
-n, m = list(map(int, input().split()))
-t1, t2 = 0, b * m
-while t1 + t2 != n:
-    t1, t2 = t1 + a, t2 - b
-print(t1, t2)
-
-# без 10, 12, 13
