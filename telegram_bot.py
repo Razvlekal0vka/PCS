13d7ff9268c3d362b40f213cd23e583b88aa0a13
Razvlekal0vka <u14111415232119518@gmail.com>
2022-04-04 23:06:22 +0300

    .

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,12 +1,10 @@
 # Импортируем необходимые классы.
 import logging
 from telegram.ext import Updater, MessageHandler, Filters
-from telegram.ext import CommandHandler
-from telegram import ReplyKeyboardMarkup
 
 # Запускаем логгирование
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING
 )
 
 logger = logging.getLogger(__name__)
@@ -33,72 +31,12 @@ def main():
     # Получаем из него диспетчер сообщений.
     dp = updater.dispatcher
 
-    reply_keyboard = [['/address', '/phone'],
-                      ['/site', '/work_time']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
-
-    # Напишем соответствующие функции.
-    # Их сигнатура и поведение аналогичны обработчикам текстовых сообщений.
-    def start(update, context):
-        update.message.reply_text(
-            "Привет! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!")
-
-    def help(update, context):
-        update.message.reply_text(
-            "Я пока не умею помогать... Я только ваше эхо.")
-
-    # Напишем соответствующие функции.
-    def help(update, context):
-        update.message.reply_text(
-            "Я бот справочник.")
-
-    def address(update, context):
-        update.message.reply_text(
-            "Адрес: г. Москва, ул. Льва Толстого, 16")
-
-    def phone(update, context):
-        update.message.reply_text("Телефон: +7(495)776-3030")
-
-    def site(update, context):
-        update.message.reply_text(
-            "Сайт: http://www.yandex.ru/company")
-
-    def work_time(update, context):
-        update.message.reply_text(
-            "Время работы: круглосуточно.")
-
-    def main():
-        updater = Updater(TOKEN)
-        dp = updater.dispatcher
-        dp.add_handler(CommandHandler("address", address))
-        dp.add_handler(CommandHandler("phone", phone))
-        dp.add_handler(CommandHandler("site", site))
-        dp.add_handler(CommandHandler("work_time", work_time))
-        dp.add_handler(CommandHandler("help", help))
-        updater.start_polling()
-
-        updater.idle()
-
-    def start(update, context):
-        update.message.reply_text(
-            "Я бот-справочник. Какая информация вам нужна?",
-            reply_markup=markup
-        )
-
-    # Зарегистрируем их в диспетчере рядом
-    # с регистрацией обработчиков текстовых сообщений.
-    # Первым параметром конструктора CommandHandler я
-    # вляется название команды.
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
-
     # Создаём обработчик сообщений типа Filters.text
     # из описанной выше функции echo()
     # После регистрации обработчика в диспетчере
     # эта функция будет вызываться при получении сообщения
     # с типом "текст", т. е. текстовых сообщений.
     text_handler = MessageHandler(Filters.text, echo)
-    print(updater.dispatcher)
 
     # Регистрируем обработчик в диспетчере.
     dp.add_handler(text_handler)
@@ -108,6 +46,7 @@ def main():
     # Ждём завершения приложения.
     # (например, получения сигнала SIG_TERM при нажатии клавиш Ctrl+C)
     updater.idle()
+
 
 # Запускаем функцию main() в случае запуска скрипта.
 if __name__ == '__main__':
