6bade6f84314cf537bc2192248580435ebcec206
Razvlekal0vka <u14111415232119518@gmail.com>
2022-04-15 11:10:29 +0300

    .

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,56 @@
 import logging
-from telegram.ext import Updater, MessageHandler, Filters
+
+from telegram import ReplyKeyboardRemove, ReplyKeyboardMarkup
 from telegram.ext import CommandHandler
+from telegram.ext import Updater, MessageHandler, Filters
+
+letters_ru = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЫыЭэЮюЯя'
+letters_en = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+letters_sy = '_-ъь[]|/.,<>?&'
+letters_nu = '0123456789'
+
+dialogues_ru = {
+    'start': "Привет! Я PCSbot, я могу помочь вам с регистрацией и её подтверждением. Также вы сможете востановить"
+             " через меня пароль от своей учетной записи, загружать и скачавать файлы, предоставлять доступ к ним и "
+             "смотреть древо своих данных",
+    'help_command': '/help - выведет вам все доступные команды',
+    'entrance_to_account_command': '/entrance_to_account - вход в аккаунт',
+    'registration_command': '/registration - регистрация нового аккаунта',
+    'password_recovery_command': '/password_recovery - восстановление пароля',
+    'account_deleting_command': '/account_deleting - удаление аккаунта',
+    'ru_command': '/ru - выбор русского языка',
+    'en_command': '/en - choice of English',
+}
+
+dialogues_en = {
+    'start': "Hey! I am PCSbot, I can help you with registration and confirmation. You can also restore"
+             " through me the password from your account, upload and download files, provide access to them and "
+             "view your data tree",
+    'help_command': '/help - will give you all available commands',
+    'entrance_to_account_command': '/entrance_to_account - entrance to account',
+    'registration_command': '/registration - registering a new account',
+    'password_recovery_command': '/password_recovery - password recovery',
+    'account_deleting_command': '/account_deleting - account deleting',
+    'ru_command': '/ru - выбор русского языка',
+    'en_command': '/en - choice of English',
+}
+
+dialogues = {'ru': dialogues_ru,
+             'en': dialogues_en
+             }
+
+language = 'en'
+
+reply_keyboard = [['/entrance_to_account'],
+                  ['/registration'],
+                  ['/password_recovery'],
+                  ['/account_deleting'],
+                  ['/ru'],
+                  ['/en'],
+                  ['/help'],
+                  ]
+
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING
@@ -12,23 +62,36 @@ TOKEN = '5227264432:AAHDTKHFiy2fE5Mx1y_UF6e-NqlBbQloDU0'
 
 
 def echo(update, context):
-    update.message.reply_text(update.message.text)
+    # update.message.reply_text(update.message.text)
+    print(update.message.text)
 
 
 def start(update, context):
-    update.message.reply_text(
-        "Привет! Я PCSbot, я могу помочь вам с регистрацией и её подтверждением. Также вы сможете востановить через"
-        " меня пароль от своей учетной записи, загружать и скачавать файлы, предоставлять доступ к ним и смотреть"
-        " древо своих данных")
-    update.message.reply_text('/help - выведет вам все доступные команды')
+    update.message.reply_text(dialogues[language]['start'])
+    update.message.reply_text(dialogues[language]['help_command'], reply_markup=markup)
 
 
 def help(update, context):
-    update.message.reply_text('/registration - регистрация нового аккаунта')
-    update.message.reply_text('/password_recovery - восстановление пароля')
-    update.message.reply_text('/account_deleting - удаление аккаунта')
-    update.message.reply_text('/help - выведет вам все доступные команды')
+    update.message.reply_text(dialogues[language]['entrance_to_account_command'])
+    update.message.reply_text(dialogues[language]['registration_command'])
+    update.message.reply_text(dialogues[language]['password_recovery_command'])
+    update.message.reply_text(dialogues[language]['account_deleting_command'])
+    update.message.reply_text(dialogues[language]['ru_command'])
+    update.message.reply_text(dialogues[language]['en_command'])
+    update.message.reply_text(dialogues[language]['help_command'])
 
+
+def en(update, context):
+    global language
+    language = 'en'
+
+
+def ru(update, context):
+    global language
+    language = 'ru'
+
+def entrance_to_account(update, context):
+    update.message.reply_text('Entrance to Account')
 
 def registration(update, context):
     update.message.reply_text('registration')
@@ -42,6 +105,13 @@ def account_deleting(update, context):
     update.message.reply_text('account_deleting')
 
 
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+
 def main():
     updater = Updater(TOKEN)
 
@@ -52,6 +122,8 @@ def main():
     dp.add_handler(CommandHandler("password_recovery", password_recovery))
     dp.add_handler(CommandHandler("account_deleting", account_deleting))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("en", en))
+    dp.add_handler(CommandHandler("ru", ru))
 
     text_handler = MessageHandler(Filters.text, echo)
 
--- xxxx.py
+++ xxxx.py
@@ -1,29 +1,100 @@
-import asyncio
-import os
-from random import randint
+import logging
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CommandHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 
-ITER_NUM = 100000
-COROUT_NUM = 9999
+reply_keyboard = [['/address', '/phone'],
+                  ['/site', '/work_time']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+
+letters_ru = 'АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЫыЭэЮюЯя'
+letters_en = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
+letters_sy = '_-ъь[]|/.,<>?&'
+letters_nu = '0123456789'
+
+dialogues_ru = {
+    'start': "Привет! Я PCSbot, я могу помочь вам с регистрацией и её подтверждением. Также вы сможете востановить"
+             " через меня пароль от своей учетной записи, загружать и скачавать файлы, предоставлять доступ к ним и "
+             "смотреть древо своих данных",
+    'help_command': '/help - выведет вам все доступные команды',
+    'registration_command': '/registration - регистрация нового аккаунта',
+    'password_recovery_command': '/password_recovery - восстановление пароля',
+    'account_deleting_command': '/account_deleting - удаление аккаунта',
+                }
+
+dialogues_eu = {
+    'start': "Hey! I am PCSbot, I can help you with registration and confirmation. You can also restore"
+             " through me the password from your account, upload and download files, provide access to them and "
+             "view your data tree",
+    'help_command': '/help - will give you all available commands',
+    'registration_command': '/registration - registering a new account',
+    'password_recovery_command': '/password_recovery - password recovery',
+    'account_deleting_command': '/account_deleting - account deleting',
+                }
+
+dialogues = {'ru': dialogues_ru,
+             'en': dialogues_eu
+             }
+
+language = 'ue'
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.WARNING
+)
+
+logger = logging.getLogger(__name__)
+
+TOKEN = '5227264432:AAHDTKHFiy2fE5Mx1y_UF6e-NqlBbQloDU0'
 
 
-async def do_some_work(i):
-    for j in range(ITER_NUM):
-        print(format_work(i, j))
-        await asyncio.sleep(0.01 * randint(1, 10))
+def help(update, context):
+    update.message.reply_text(
+        "Я бот справочник.")
 
 
-async def main():
-    tasks = []
-    for i in range(COROUT_NUM):
-        tasks.append(asyncio.create_task(do_some_work(i)))
-    await asyncio.gather(*tasks)
+def address(update, context):
+    update.message.reply_text(
+        "Адрес: г. Москва, ул. Льва Толстого, 16")
 
 
-def format_work(i, j):
-    return i * ' ' + "█" + (COROUT_NUM - i) * ' ' + f'cr {i} iter {j} ' + "■" * j
+def phone(update, context):
+    update.message.reply_text("Телефон: +7(495)776-3030")
+
+
+def site(update, context):
+    update.message.reply_text(
+        "Сайт: http://www.yandex.ru/company")
+
+
+def work_time(update, context):
+    update.message.reply_text(
+        "Время работы: круглосуточно.")
+
+def start(update, context):
+    update.message.reply_text(
+        "Я бот-справочник. Какая информация вам нужна?",
+        reply_markup=markup
+    )
+def close_keyboard(update, context):
+    update.message.reply_text(
+        "Ok",
+        reply_markup=ReplyKeyboardRemove()
+    )
+
+def main():
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("address", address))
+    dp.add_handler(CommandHandler("phone", phone))
+    dp.add_handler(CommandHandler("site", site))
+    dp.add_handler(CommandHandler("work_time", work_time))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("close", close_keyboard))
+    updater.start_polling()
+
+    updater.idle()
 
 
 if __name__ == '__main__':
-    if os.name == 'nt':
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    asyncio.run(main())
+    main()
